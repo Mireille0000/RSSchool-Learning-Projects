6d37fa002e00aac28820c3e2b98da61a65c1b64e
Gasanova Tamara <solas93@mail.ru>
2023-12-08 21:00:17 +0300

    feat: implement manual switching for mobile devices

--- coffee-house/index.js
+++ coffee-house/index.js
@@ -48,7 +48,7 @@ function activateDashes (index) {
 //     })
 // })
 //
-function navigateSlider () {
+function navigateSlider() {
     if (margin > -200 && dashIndex < 3) {
         dashIndex++;
         margin -= 100;
@@ -62,10 +62,7 @@ function navigateSlider () {
     }
 }
 
-rightArrowButton.addEventListener('click', navigateSlider);
-
-leftArrowButton.addEventListener('click', () => {
-    console.log('hey')
+function navigateSliderLeft() {
     if (margin >= -200 && margin < 0) {
         margin += 100;
         coffeeSlider.style.marginLeft = margin + '%';
@@ -76,10 +73,12 @@ leftArrowButton.addEventListener('click', () => {
         margin -= 200;
         coffeeSlider.style.marginLeft = margin + '%';
     }
-    console.log(margin)
-})
+}
 
-let interval = setInterval(navigateSlider, 3000);
+rightArrowButton.addEventListener('click', navigateSlider);
+leftArrowButton.addEventListener('click', navigateSliderLeft);
+
+let interval = setInterval(navigateSlider, 7000);
 
 class mouseEvent {
     handleEvent(event) {
@@ -88,22 +87,76 @@ class mouseEvent {
             clearInterval(interval);
             break;
         case 'mouseleave':
-            interval = setInterval(navigateSlider, 3000);
+            interval = setInterval(navigateSlider, 7000);
+        break;
+        case 'touchstart':
+            clearInterval(interval);
+            break;
+        case 'touchend':
+            interval = setInterval(navigateSlider, 7000);
         break;
       }
     }
   }
 
-let mouse= new mouseEvent();
+let mouse = new mouseEvent();
 
 sliderWrapper.addEventListener("mouseenter", mouse);
 sliderWrapper.addEventListener("mouseleave", mouse);
 
+// swipe for the slider (mobiles devices only)
+
+sliderWrapper.addEventListener('touchstart', touchStart, false);
+sliderWrapper.addEventListener('touchmove', touchMove, false);
+
+let x1 = null;
+let y1 = null;
+
+function touchStart(event) {
+    const firstTouch = event.touches[0];
+    x1 = Math.round(firstTouch.clientX);
+    y1 = Math.round(firstTouch.clientY);
+}
+
+function touchMove(event) {
+    if (x1 === 0 || y1 === 0) {
+        return false;
+    }
+
+    let x2 = Math.round(event.touches[0].clientX);
+    let y2 = Math.round(event.touches[0].clientY);
+
+    let coordinatesDiffX = x2 - x1;
+    let coordinatesDiffY = y2 - y1;
+
+    if (Math.abs(coordinatesDiffX) > Math.abs(coordinatesDiffY)) {
+        if (coordinatesDiffX > 0) {
+            navigateSlider();
+            console.log('right')
+        } else {
+            navigateSliderLeft();
+            console.log('left')
+        }
+    } else {
+        if (coordinatesDiffY > 0) {
+            navigateSlider();
+            console.log('bottom')
+        } else {
+            navigateSliderLeft();
+            console.log('top')
+        }
+    }
+
+    x1 = null;
+    y1 = null;
+}
 
 
+sliderWrapper.addEventListener("touchstart", mouse);
+sliderWrapper.addEventListener("touchend", mouse);
 
 
 console.log(`Coffee house, week 1, score: 100/96(картинки в секциях enjoy и mobile apps расходятся более чем на 10px`);
 console.log(`Coffee house, week 2, score: 90/ 90(все пункты выполнены, есть расхождения по перфект пикселю, но они не превышают установленные 10px`);
 
-// сonsole.log(`accomplish the slider`);
+// сonsole.log(`accomplish the slider: adjust it for mobile version + touch + progress bar`);
