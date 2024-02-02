e33bb7ae4ac68a234e9d079bab53ce3e90165209
Gasanova Tamara <solas93@mail.ru>
2024-02-02 16:31:42 +0300

    feat: change game field and hints styles

--- nonograms/nonograms-main.js
+++ nonograms/nonograms-main.js
@@ -1,41 +1,75 @@
 console.log("Let's get started")
 
 // create grid
-document.body.onload = addElement;
 
-function addElement() {
+document.body.onload = addElements;
+
+function addElements() {
   const title = document.createElement("h1");
   title.innerHTML = "Nonograms";
+
+  const attempts = document.createElement("div");
+  attempts.className = "attempts";
+
+  let lives = "&#x2665 &#x2665 &#x2665";
+  attempts.innerHTML = lives;
+
   const grid = document.createElement("div");
   grid.className = "grid";
   const cell = document.createElement("div");
   cell.className = "cell";
   const hints = document.createElement("div");
   hints.className = "hint";
-  document.body.prepend(title, grid);
 
-  for (let i = 0; i < 6; ++i) {
-    grid.appendChild(hints.cloneNode(true));
-    hints.innerHTML  = 0;
-  }
+  
+  const horizontalHints = document.createElement("div");
+  const horizontalHintsItems = document.createElement("div");
+  horizontalHintsItems.className = "hint-horizontal";
+  horizontalHints.className = "hint-horisontal-box";
+
+  const verticalHints = document.createElement("div");
+  const verticalHintsItems = document.createElement("div");
+  verticalHintsItems.className = "hint-vertical";
+  verticalHints.className = "hint-vertical-box";
+
+  document.body.prepend(title, attempts, horizontalHints, grid);
 
   for (let i = 0; i < 30; i++) {
     grid.appendChild(cell.cloneNode(true));
   }
+  // grid.after(verticalHints);
 
+  // for (let i = 0; i < 5; i++) {
+  //   verticalHints.prepend(verticalHintsItems.cloneNode(true));
+  // }
+
+   // change
   const test = Array.from(document.querySelectorAll(".cell"));
-  console.log(test[0]);
-  console.log(test[6]);
-  console.log(test[12]);
-  console.log(test[18]);
-  console.log(test[24]);
+
+  test.forEach((cell) => {
+    cell.style = "cursor: pointer";
+    cell.addEventListener("click", () => {
+      cell.style = "background-color: #000; pointer-events: none";
+    });
+  })
+
+  // horizontal hints
+  for (let i = 0; i < 5; i++) {
+    horizontalHints.prepend(horizontalHintsItems.cloneNode(true));
+  }
+
+
+  // vertical hints
   test.map((item) => {
-    if (test.indexOf(item) % 6 ===0 ) {
+    if (test.indexOf(item) % 6 === 0) {
       item.className = "hint-vertical";
-      item.innerHTML = 0;
+      console.log(test.indexOf(item));
     }
   })
 }
 
-const grid = document.querySelector(".grid");
-console.log(grid)
+// todo
+// show hints that are necessary to make an image (how they shoud be made??)
+// add class to clicked cells
+// - if a cell is correct it becomes black
+// - if a cell is incorrect, a crosse appears in the cell
