b3242c4535a4a6a3e9bb736c164fb97949a4d6d2
Gasanova Tamara <solas93@mail.ru>
2024-02-24 03:52:00 +0300

    feat: implement ts and config eslint and webpack

--- news-JS-main/webpack.config.js
+++ news-JS-main/webpack.config.js
@@ -5,7 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const DotenvWebpackPlugin = require('dotenv-webpack');
 
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: path.resolve(__dirname, './src/index'),
     mode: 'development',
     module: {
         rules: [
@@ -13,10 +13,15 @@ const baseConfig = {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
+
+            { 
+                test: /\.ts$/i, 
+                use: 'ts-loader',
+            }
         ],
     },
     resolve: {
-        extensions: ['.js'],
+        extensions: ['.ts', '.js'],
     },
     output: {
         filename: 'index.js',
@@ -29,6 +34,7 @@ const baseConfig = {
             filename: 'index.html',
         }),
         new CleanWebpackPlugin(),
+        new EslingPlugin({ extensions: 'ts' })
     ],
 };
 
@@ -38,3 +44,5 @@ module.exports = ({ mode }) => {
 
     return merge(baseConfig, envConfig);
 };
+
+const EslingPlugin = require('eslint-webpack-plugin')
