eb60bfb48e2f65bb8b83177df08d7b6d2028d172
Gasanova Tamara <solas93@mail.ru>
2024-03-12 01:22:59 +0300

    feat: RSS-PZ-01 add font style

--- puzzle-app/src/entry-page/log-in-form.ts
+++ puzzle-app/src/entry-page/log-in-form.ts
@@ -5,10 +5,12 @@ export class LogInForm {
   constructor() {
     this.input = document.createElement('input') as HTMLInputElement;
     this.input.className = 'entry-page-input';
+    this.input.type = 'required';
 
     this.button = document.createElement('button') as HTMLButtonElement;
     this.button.className = 'log-in-button';
     this.button.innerHTML = 'Log In';
+    this.button.type = 'submit';
   }
 
   renderEntryPage() {
@@ -16,5 +18,10 @@ export class LogInForm {
     container.className = 'container';
     document.body.append(container);
     container.append(this.input, this.input.cloneNode(true), this.button);
+    const inputText: HTMLInputElement[] = Array.from(
+      document.querySelectorAll('.entry-page-input'),
+    );
+    inputText[0].placeholder = 'First name';
+    inputText[1].placeholder = 'Last name';
   }
 }
