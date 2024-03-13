4c198dd380459d596b8381b4b38cd24e7fe33305
Mireille0000 <111881923+Mireille0000@users.noreply.github.com>
2024-03-13 14:32:20 +0300

    Feat/rss pz 01 implement login form (#18)
    * feat: RSS-PZ-01 create LogInForm class and style the rendered form
    
    * feat: RSS-PZ-01 add font style
    
    * feat: RSS-PZ-01 add background image, add title and app description, update app architecture
    
    * feat: RSS-PZ-01 make input fields required, update class EntryPage
    
    * feat: RSS-PZ-01 make input fields required, update class EntryPage

---
+++ puzzle-app/src/app/entry-page/entry-page.ts
@@ -0,0 +1,57 @@
+const pageWrapper = document.createElement('div');
+
+export default class EntryPage {
+  firstName: HTMLInputElement;
+
+  lastName: HTMLInputElement;
+
+  button: HTMLButtonElement;
+
+  header: HTMLHeadingElement;
+
+  text: HTMLParagraphElement;
+
+  constructor() {
+    this.firstName = document.createElement('input') as HTMLInputElement;
+    this.firstName.className = 'entry-page-input';
+    this.firstName.type = 'text';
+
+    this.lastName = document.createElement('input') as HTMLInputElement;
+    this.lastName.className = 'entry-page-input';
+    this.lastName.type = 'text';
+    this.firstName = document.createElement('input') as HTMLInputElement;
+    this.firstName.className = 'entry-page-input';
+    this.firstName.type = 'text';
+
+    this.lastName = document.createElement('input') as HTMLInputElement;
+    this.lastName.className = 'entry-page-input';
+    this.lastName.type = 'text';
+
+    this.header = document.createElement('h1');
+    this.header.innerHTML = 'Puzzle (Learn English)';
+    this.text = document.createElement('p');
+    this.text.innerHTML = 'Click on words. Collect phrases. Improve your English';
+    this.button = document.createElement('button') as HTMLButtonElement;
+    this.button.className = 'log-in-button';
+    this.button.innerHTML = 'Log In';
+    this.button.type = 'submit';
+  }
+
+  renderEntryPage() {
+    pageWrapper.className = 'page-wrapper';
+    document.body.append(pageWrapper);
+    const container = document.createElement('div') as HTMLElement;
+    container.className = 'container';
+    pageWrapper.append(this.header, this.text, container);
+    container.append(this.firstName, this.lastName, this.button);
+    this.firstName.placeholder = 'First name';
+    this.lastName.placeholder = 'Last name';
+    this.firstName.required = true;
+    this.lastName.required = true;
+    container.append(this.firstName, this.lastName, this.button);
+    this.firstName.placeholder = 'First name';
+    this.lastName.placeholder = 'Last name';
+    this.firstName.required = true;
+    this.lastName.required = true;
+  }
+}
--- puzzle-app/src/index.ts
+++ puzzle-app/src/index.ts
@@ -1,1 +1,4 @@
-console.log('Hello World!');
+import EntryPage from './app/entry-page/entry-page.ts';
+import './styles.css';
+
+new EntryPage().renderEntryPage();
