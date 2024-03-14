c4766ede1b8cd7135a423dd9a6e7e557b1bb2fde
Mireille0000 <111881923+Mireille0000@users.noreply.github.com>
2024-03-14 16:32:07 +0300

    Feat/rss pz 02 implement form validation (#19)
    * feat: RSS-PZ-02 validate inputs(language, number of characters, first letter in upper case
    
    * refactor: RSS-PZ-02 delete unnecessary code

--- puzzle-app/src/app/entry-page/entry-page.ts
+++ puzzle-app/src/app/entry-page/entry-page.ts
@@ -1,17 +1,23 @@
 const pageWrapper = document.createElement('div');
 
 export default class EntryPage {
+  header: HTMLHeadingElement;
+
+  text: HTMLParagraphElement;
+
+  form: HTMLFormElement;
+
   firstName: HTMLInputElement;
 
   lastName: HTMLInputElement;
 
   button: HTMLButtonElement;
 
-  header: HTMLHeadingElement;
-
-  text: HTMLParagraphElement;
-
   constructor() {
+    this.form = document.createElement('form') as HTMLFormElement;
+    this.form.action = '/';
+    this.form.method = 'GET';
+
     this.firstName = document.createElement('input') as HTMLInputElement;
     this.firstName.className = 'entry-page-input';
     this.firstName.type = 'text';
@@ -29,8 +35,10 @@ export default class EntryPage {
 
     this.header = document.createElement('h1');
     this.header.innerHTML = 'Puzzle (Learn English)';
+
     this.text = document.createElement('p');
     this.text.innerHTML = 'Click on words. Collect phrases. Improve your English';
+
     this.button = document.createElement('button') as HTMLButtonElement;
     this.button.className = 'log-in-button';
     this.button.innerHTML = 'Log In';
@@ -40,18 +48,28 @@ export default class EntryPage {
   renderEntryPage() {
     pageWrapper.className = 'page-wrapper';
     document.body.append(pageWrapper);
+    pageWrapper.append(this.header, this.text, this.form);
+
     const container = document.createElement('div') as HTMLElement;
     container.className = 'container';
-    pageWrapper.append(this.header, this.text, container);
-    container.append(this.firstName, this.lastName, this.button);
+
+    const validLen = document.createElement('div');
+    validLen.innerHTML = 'Minimum characters number: first name - 3, last name - 4';
+    validLen.className = 'validation-hint length';
+
+    const validCap = document.createElement('div');
+    validCap.innerHTML = 'The first letter of each field should be in upper case';
+    validCap.className = 'validation-hint first-letter';
+
+    const validLang = document.createElement('div');
+    validLang.innerHTML = 'Characters of latin alphabet';
+    validLang.className = 'validation-hint alphabet';
+
     this.firstName.placeholder = 'First name';
     this.lastName.placeholder = 'Last name';
     this.firstName.required = true;
     this.lastName.required = true;
-    container.append(this.firstName, this.lastName, this.button);
-    this.firstName.placeholder = 'First name';
-    this.lastName.placeholder = 'Last name';
-    this.firstName.required = true;
-    this.lastName.required = true;
+    this.form.append(container);
+    container.append(this.firstName, this.lastName, validLen, validCap, validLang, this.button);
   }
 }
