407cc9ec70519287d9eaa38a05b5a91026c728e8
Gasanova Tamara <solas93@mail.ru>
2024-03-18 17:34:07 +0300

    feat: RSS-PZ-08 create puzzle field and words block

--- puzzle-app/src/app/main-page/main-app-page.ts
+++ puzzle-app/src/app/main-page/main-app-page.ts
@@ -1,4 +1,6 @@
 import Page from '../templates/page.ts';
+import puzzleIncon from '../../images/puzzle1.svg';
+import settingsIcon from '../../images/settings.svg';
 
 export default class MainAppPage extends Page {
   // nav bar (hints, sound, settings, icon puzzle ,round/level?)
@@ -13,6 +15,8 @@ export default class MainAppPage extends Page {
 
   puzzle?: HTMLSpanElement;
 
+  words: HTMLDivElement;
+
   constructor() {
     super();
     this.puzzleField = document.createElement('div');
@@ -20,10 +24,35 @@ export default class MainAppPage extends Page {
 
     this.puzzleLine = document.createElement('div');
     this.puzzleLine.className = 'puzzle-line';
+
+    this.words = document.createElement('div');
+    this.words.className = 'words';
   }
 
   renderMainPage() {
-    console.log('Hey');
     document.body.append(this.pageWrapper);
+    this.pageWrapper.className = 'main-page-wrapper';
+    const wrapper = document.querySelector('.main-page-wrapper') as HTMLDivElement;
+    this.pageWrapper.append(this.header, this.contentWrapper);
+
+    const content = document.querySelector('.page-content') as HTMLDivElement;
+    content.className = 'main-content-wrapper';
+    const header = document.querySelector('header') as HTMLElement;
+
+    header.append();
+    content.append(this.puzzleField, this.words);
+
+    const icon = new Image();
+    icon.src = puzzleIncon;
+    icon.alt = 'puzzle';
+
+    const settings = new Image();
+    settings.src = settingsIcon;
+    settings.alt = 'settings';
+    settings.className = 'settings';
+
+    header.append(icon, settings);
+
+    // const appField = document.querySelector('.puzzle-field')
   }
 }
